793afc13e78d0ab41b1cd9539ce975d9e1aa9611
Gato de Metal <lilianaballesteros@Lilianas-MacBook.local>
2017-01-12 20:19:43 -0500

    last branch

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
     var $menu = $("#sidebar-wrapper");
 
-    $(document)
-      .on("click", ".js-menu-open", function(evt)
-      {$menu.addClass("open");
+    $(".js-menu-open").on("click", function(evt){
+      $menu.addClass("open");
 
       return evt.target.tagName === "A";
-    })
-      .on("click", ".js-menu-close", function(evt)
-      {$menu.removeClass("open");
+    });
+
+    $(".js-menu-close").on("click", function(evt){
+      $menu.removeClass("open");
 
       return evt.target.tagName === "A";
     });
